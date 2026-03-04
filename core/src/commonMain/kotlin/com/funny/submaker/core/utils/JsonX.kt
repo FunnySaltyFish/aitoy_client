39e7949598da932ca5b834e49631ba121d1c3dc9
FunnySaltyFish <3036548996@qq.com>
2026-03-04 23:23:07 +0800

    feat: JsonX 新增 safeFromJson 拓展函数

--- core/src/commonMain/kotlin/com/funny/submaker/core/utils/JsonX.kt
+++ core/src/commonMain/kotlin/com/funny/submaker/core/utils/JsonX.kt
@@ -1,5 +1,6 @@
 package com.funny.submaker.core.utils
 
+import com.funny.submaker.core.log.Log
 import kotlinx.serialization.json.Json
 
 object JsonX {
@@ -12,5 +13,11 @@ object JsonX {
 }
 
 inline fun <reified T> String.fromJson(): T = JsonX.json.decodeFromString(this)
+inline fun <reified T> String.safeFromJson(default: T): T = runCatching {
+    JsonX.json.decodeFromString<T>(this)
+}.getOrElse {
+    Log.e("JsonX") { "Failed to parse JSON: $this" }
+    default
+}
 
 inline fun <reified T> T.toJson(): String = JsonX.json.encodeToString(this)
